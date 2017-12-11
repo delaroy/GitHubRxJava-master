f5ccc4c7d317fa15e9f6a2126b03395abb567c4b
Delaroy Studios <bamoyk@yahoo.com>
2017-12-11 12:31:23 +0100

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/githubrxjava/GitHubService.java
@@ -0,0 +1,11 @@
+package com.delaroystudios.githubrxjava;
+
+import java.util.List;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+
+public interface GitHubService {
+    @GET("users/{user}/starred") Observable<List<GitHubRepo>> getStarredRepositories(@Path("user") String userName);
+}
